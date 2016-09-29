27f1699d732d26ac87fbfcecbadf049a4f697244
Ivan <ivan.herndon@gmail.com>
2016-09-29 15:32:07 -0400

    implementing fileserver and tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,47 @@
-/* global describe it */
+/* global describe it before __dirname after */
+
+'use strict';
 const assert = require('assert');
+const path = require('path');
+const request = require('request');
+const binstaface = require('../lib');
 
 describe('my app test', function() {
-    it('does something', function() {
-        assert.ok(true, 'Did something');
+  const PORT = 4455;
+  let server;
+
+  before(function(done) {
+    server = binstaface(path.join(__dirname, '..'))
+      .listen(PORT, () => done());
+  });
+
+  after(function(done) {
+    server.close(() => done());
+  });
+
+  it('sends 404 for non-existing files', function(done) {
+    request(`http://localhost:${PORT}/dummy.txt`, (error, res, body) => {
+      if(error) {
+        return done(error);
+      }
+
+      assert.equal(res.statusCode, 404);
+      assert.equal(body, 'THERE IS A PROBLEM WITH THE FILE YOU SPECIFIED');
+      done();
     });
+  });
+
+  it('sends package.json', function(done) {
+    request({
+      url: `http://localhost:${PORT}/package.json`,
+      json: true
+    }, (error, res, body) => {
+      if(error) {
+        return done(error);
+      }
+
+      assert(body.description, 'A picture sharing application!');
+      done();
+    });
+  });
 });
